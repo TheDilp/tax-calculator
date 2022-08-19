e5a00f95afaa5a0fc15657dd1565f34ffd7e6ca7
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 14:27:44 +0200

    Update income type with buttons

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { IncomeContext } from "../contexts/IncomeContext";
 
 export default function ToggleButton({ title, type }) {
-  const { incomeData } = useContext(IncomeContext);
+  const { incomeData, dispatch } = useContext(IncomeContext);
   return (
     <button
       className={`${
         incomeData.incomeType === type ? "bg-blue-500 text-white" : ""
       } shadow-sm rounded-lg py-1 w-1/2 text-lg border-slate-100 border-2 hover:text-blue-400 active:bg-blue-500 active:text-white active:shadow-none transition-colors`}
+      onClick={() => dispatch({ type: "CHANGE_TYPE", payload: type })}
     >
       {title}
     </button>
