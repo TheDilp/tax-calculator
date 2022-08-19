129bf80bfd4be1b934648e17c31e08a94d946267
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 11:52:53 +0200

    Styling for input title and amount input

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function Inputs() {
   return (
-    <div className="w-1/2 bg-red-500">
-      <h2>What is your total income?</h2>
+    <div className="w-1/2 border-r-2 border-gray-300 p-4">
+      <h2 className="text-center text-xl font-semibold mb-2">
+        What is your total income?
+      </h2>
       <div>
-        <div>
-          <input type="text" placeholder="e.g 12,000" />
+        <div className="flex border-2 border-blue-400 rounded-lg p-1">
+          <input
+            className="flex-1 outline-none appearance-none"
+            type="number"
+            placeholder="e.g 12,000"
+          />
           <select defaultValue={"Monthly"}>
             <option value="Weekly">Weekly</option>
             <option value="Fortnightly">Fortnightly</option>
