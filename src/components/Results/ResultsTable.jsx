889d9c23c4ccff5e4ecab84a5b8405d15366caac
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 12:58:09 +0200

    Fixed class->className; removed useless style link

--- src/components/Results/ResultsTable.jsx
+++ src/components/Results/ResultsTable.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ResultsTable() {
   const frequencies = ["Weekly", "Fortnightly", "Monthly", "Annually"];
   return (
-    <table class="w-full text-center">
+    <table className="w-full text-center">
       <thead>
         <tr className="border-b-2 border-gray-100">
           <th className="text-left">Frequency</th>
