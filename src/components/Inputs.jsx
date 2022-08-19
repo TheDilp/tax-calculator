bea3bc683fe0e9beb6a48b46e5da59a30781501e
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 12:14:24 +0200

    Updated calc button style

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -33,7 +33,7 @@ export default function Inputs() {
         </div>
       </div>
       <div className="flex w-full">
-        <button className="w-1/2 mx-auto shadow-sm rounded-lg py-2 text-lg border-slate-100 border-2 bg-blue-500 text-white hover:text-blue-400  active:shadow-none transition-colors">
+        <button className="w-1/2 mx-auto shadow-sm rounded-lg py-2 text-lg border-slate-100 border-2 bg-blue-500 text-white hover:text-blue-200 active:bg-blue-200 active:text-white active:shadow-none transition-colors">
           Calculate
         </button>
       </div>
