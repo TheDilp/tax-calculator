03e542fda65023c177ec37c9c2723ae55b9e6450
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 11:46:36 +0200

    Basic layout for input side

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function Inputs() {
-  return <div className="w-1/2 bg-red-500">a</div>;
+  return (
+    <div className="w-1/2 bg-red-500">
+      <h2>What is your total income?</h2>
+      <div>
+        <div>
+          <input type="text" placeholder="e.g 12,000" />
+          <select defaultValue={"Monthly"}>
+            <option value="Weekly">Weekly</option>
+            <option value="Fortnightly">Fortnightly</option>
+            <option value="Monthly">Monthly</option>
+            <option value="Annually">Annually</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <h2>Please choose the income type</h2>
+        <div className="space-x-4">
+          <button>Gross Income</button>
+          <button>Net Income</button>
+        </div>
+      </div>
+      <div>
+        <button>Calculate</button>
+      </div>
+    </div>
+  );
 }
