10968e1d8e7cb7fe94205b0c34c52eebeed299b2
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 11:41:44 +0200

    Stylized title of wrapper and added inputs part

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Inputs() {
+  return <div className="w-1/2 bg-red-500">a</div>;
+}
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Inputs from "./Inputs";
 
 export default function Wrapper() {
   return (
-    <div className="w-1/2 flex flex-row shadow-md rounded-sm h-96">
-      <div className="w-full">
-        <h1>Income Tax Calculator</h1>
+    <div className="w-1/2 flex flex-row flex-wrap content-start shadow-md rounded-sm h-96">
+      <div className="w-full h-10">
+        <h1 className="text-3xl font-semibold text-center">
+          Income Tax Calculator
+        </h1>
+      </div>
+      <div className="w-full flex flex-row h-86">
+        <Inputs />
       </div>
     </div>
   );
