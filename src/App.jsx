e8a4500bd3ed3e5a1f1d69875b40dae7d5e86b59
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 11:11:57 +0200

    Init React setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>React Page </h1>
+        <br />
+        <a
+          className="button-line"
+          href="https://github.com/deityhub"
+          target="_blank"
+        >
+          Know more now
+        </a>
+      </div>
+    </div>
+  );
+}
