ea5036ee3ab81c3c38b590ec4edff046e5a93622
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 11:30:26 +0200

    Switched to createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import "../public/styles/index.css";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
