ffae2955bda1508da1e6ad58db3c0ac897622ac4
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 12:59:53 +0200

    Fixed context not exporting object

--- src/components/Inputs/IncomeInput.jsx
+++ src/components/Inputs/IncomeInput.jsx
@@ -2,13 +2,16 @@ import React, { useContext } from "react";
 import { IncomeContext } from "../../contexts/IncomeContext";
 
 export default function IncomeInput() {
-  const { income } = useContext(IncomeContext);
+  const { income, incomeType, setIncome, setIncomeType } =
+    useContext(IncomeContext);
   return (
     <div className="flex border-2 border-gray-200 focus-within:border-blue-400 rounded-md h-10">
       <input
         className="flex-1 py-1 px-2 outline-none appearance-none my-2"
         type="number"
         placeholder="e.g 12,000"
+        value={income}
+        onChange={(e) => setIncome(parseFloat(e.target.value))}
       />
       <select
         className="outline-none bg-gray-300 text-gray-600 px-2"
--- src/contexts/IncomeContext.jsx
+++ src/contexts/IncomeContext.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const IncomeContext = createContext();
 
 export default function IncomeProvider({ children }) {
-  const [income, setIncome] = useState(0);
+  const [income, setIncome] = useState(undefined);
   const [incomeType, setIncomeType] = useState("gross");
 
   return (
     <IncomeContext.Provider
-      value={(income, incomeType, setIncome, setIncomeType)}
+      value={{ income, incomeType, setIncome, setIncomeType }}
     >
       {children}
     </IncomeContext.Provider>
