b08697241c9393ee26127bff2dfac752cd62833f
Arctur <mihailo.dilparic@gmail.com>
2022-08-19 12:22:12 +0200

    Updated styles for tax input

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -8,13 +8,16 @@ export default function Inputs() {
         <h2 className="text-center text-xl font-semibold">
           What is your total income?
         </h2>
-        <div className="flex border-2 border-blue-400 rounded-lg p-1">
+        <div className="flex border-2 border-gray-200 focus-within:border-blue-400 rounded-md h-10">
           <input
-            className="flex-1 py-1 px-2 outline-none appearance-none"
+            className="flex-1 py-1 px-2 outline-none appearance-none my-2"
             type="number"
             placeholder="e.g 12,000"
           />
-          <select className="outline-none" defaultValue={"Monthly"}>
+          <select
+            className="outline-none bg-gray-300 text-gray-600 px-2"
+            defaultValue={"Monthly"}
+          >
             <option value="Weekly">Weekly</option>
             <option value="Fortnightly">Fortnightly</option>
             <option value="Monthly">Monthly</option>
