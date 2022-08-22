1e40c828cb9c78d7db254154614f138ec6620e09
Arctur <mihailo.dilparic@gmail.com>
2022-08-22 11:28:35 +0200

    Added tabs on responsive layout

--- src/components/Results/EnterIncomeWarn.jsx
+++ src/components/Results/EnterIncomeWarn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function EnterIncomeWarn() {
   return (
-    <div className="w-full lg:w-1/2 p-4 hidden lg:flex justify-center items-center">
+    <div className="w-full lg:w-1/2 p-4 flex justify-center items-center">
       <h1 className="text-xl p-4 italic border-2 border-blue-300 border-opacity-40 rounded-md">
         Please enter your income
       </h1>
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import IncomeProvider from "../contexts/IncomeContext";
 import Footer from "./Footer/Footer";
 import InputsWrapper from "./Inputs/InputsWrapper";
 import ResultsWrapper from "./Results/ResultsWrapper";
 
 export default function Wrapper() {
+  const [page, setPage] = useState(0);
   return (
     <div className="sm:w-full lg:w-1/2 flex flex-row flex-wrap content-start shadow-md rounded-sm">
       <div className="w-full h-10 pb-10 ">
         <h1 className="hidden lg:block text-3xl font-semibold text-center border-b-2">
           Income Tax Calculator
         </h1>
-        <span className="lg:hidden w-full h-10 flex items-center">
-          <div className="w-1/2 bg-blue-300 border-r-2 h-full flex justify-center items-center">
+        <div className="lg:hidden w-full flex items-center">
+          <div
+            className={`w-1/2 ${
+              page === 0 ? "bg-blue-300 border-b-2" : ""
+            }  h-full flex justify-center items-center py-2 border-blue-500 cursor-pointer transition-all`}
+            onClick={() => setPage(0)}
+          >
             <h3 className="text-xl font-bold">Income</h3>
           </div>
-          <div className="w-1/2 h-full flex justify-center items-center">
+          <div
+            className={`w-1/2 ${
+              page === 1 ? "bg-blue-300 border-b-2" : ""
+            }   h-full flex justify-center items-center py-2 border-blue-500 cursor-pointer transition-all`}
+            onClick={() => setPage(1)}
+          >
             <h3 className="text-xl font-bold">Income Details</h3>
           </div>
-        </span>
+        </div>
       </div>
       <div className="w-full flex flex-row h-86">
         <IncomeProvider>
-          <InputsWrapper />
-          <ResultsWrapper />
+          {page === 0 && <InputsWrapper />}
+          {page === 1 && <ResultsWrapper />}
         </IncomeProvider>
       </div>
       <div className="w-full">
